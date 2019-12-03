389cd46b17b66a9833c8dbbf19217754f252ece4
Michael Weedman <michael.weedman@gmail.com>
2019-12-03 09:42:55 -0700

    More filling in

--- app.js
+++ app.js
@@ -10,24 +10,39 @@ app.use(cors());
 
 app.get('/apps', (req, res) => {
   const {sort, genres} = req.query;
-  if(sort){
-    if(!['rating', 'app'].includes(sort)) {
-      //some stuff in here//
-    }
-  }
 
-  if(genres){
-    if(!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genres)){
-      //some stuff in here
-    }
-  }
+  // if(sort){
+  //   if(!['rating', 'app'].includes(sort)) {
+  //     return res.json();
+  //   }
+  // }
+
+  // if(genres){
+  //   if(!['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'].includes(genres)){
+  //     return res
+  //       .status(400)
+  //       .send('Genre must be one of the following: Action, Puzzle, Strategy, Casual, Arcade, or Card.');
+  //   }
+  //   return res.json();
+  // }
 
   let results = playstore
     .filter(singleApp => 
       singleApp
         .App
         .toLowerCase()
-        .includes(sort.toLowercase())
+        .includes(sort.toLowerCase())
     );
-  res.json(results);
+  if(sort){
+    results
+      .sort((a,b) => {
+        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+      });
+  }
+  res.
+    json(results);
+});
+
+app.listen(8000, () => {
+  console.log('Express server is listening on port 8000!');
 });
